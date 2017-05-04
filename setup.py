d5c73b4b0846d7f98f555945c370c6afa1153dac
SamuelGabriel <muelsamu@student.ethz.ch>
2017-05-05 00:49:46 +0200

    hi

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
     name = 'markout',
-    version = '1.0',
+    version = '1.1',
     description = 'Print Your Output using markdown beautifully!',
     author = 'Samuel Mueller',
     author_email = 'samuel.g.mueller@web.de',
     url = 'https://github.com/SamuelGabriel/markout', # use the URL to the github repo
-    download_url = 'https://github.com/SamuelGabriel/markout/archive/1.0.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/SamuelGabriel/markout/archive/1.1.tar.gz', # I'll explain this in a second
     keywords = ['testing', 'logging', 'markdown', 'beatiful'], # arbitrary keywords
     classifiers = [],
     install_requires=['markdown2'],
