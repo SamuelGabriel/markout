1b335b4205533152b8ccd1dd355313237f860aff
SamuelGabriel <muelsamu@student.ethz.ch>
2017-05-18 15:43:06 +0200

    corrected version number again

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
     name = 'markout',
-    version = '1.1',
+    version = '1.2',
     description = 'Print Your Output using markdown beautifully!',
     author = 'Samuel Mueller',
     author_email = 'samuel.g.mueller@web.de',
     url = 'https://github.com/SamuelGabriel/markout', # use the URL to the github repo
-    download_url = 'https://github.com/SamuelGabriel/markout/archive/1.1.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/SamuelGabriel/markout/archive/1.22.tar.gz', # I'll explain this in a second
     keywords = ['testing', 'logging', 'markdown', 'beatiful'], # arbitrary keywords
     classifiers = [],
     install_requires=['markdown2'],
